cf8c8fff9f46559c1f73f32efdda454284283494
vitaliy <vitaliynif@gmail.com>
2023-04-04 00:08:23 +0700

    project apollo init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,29 @@
-import { createApp } from "vue";
+import { createApp, provide, h } from "vue";
+import { DefaultApolloClient } from '@vue/apollo-composable'
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client/core";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+// HTTP connection to the API
+const httpLink = createHttpLink({
+  // You should use an absolute URL here
+  uri: "https://rickandmortyapi.com/graphql/",
+});
+
+const cache = new InMemoryCache();
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+const app = createApp({
+  setup() {
+    provide(DefaultApolloClient, apolloClient);
+  },
+
+  render: () => h(App),
+}).mount("#app");
